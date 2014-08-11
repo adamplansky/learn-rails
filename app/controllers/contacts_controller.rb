bf84cb5d7033918c15a265b0ae93383b326e43ce
Adam Plansky <adamplansky@gmail.com>
2014-08-11 23:25:49 +0200

    contact form

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,25 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+  
+  def create
+    #Rails.logger.debug "DEBUG: #{@contact.name}"
+    @contact = Contact.new(secure_param)
+    Rails.logger.debug "DEBUG: #{@contact.name}"
+    if @contact.valid?
+      #TODO save data
+      #TODO send message
+      
+      flash[:notice] = "Message sent from #{@contact.name}"
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  def secure_param
+    params.require(:contact).permit(:name, :email, :content)
+  end
+end
