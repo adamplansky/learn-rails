56d21fd1aba95b1d25e9815ff4bf713da3e40c75
Adam Plansky <adamplansky@gmail.com>
2014-08-13 18:55:58 +0200

    debug puts added

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,8 @@ class UserMailer < ActionMailer::Base
   
   def contact_email(contact)
     @contact = contact
+    puts Rails.application.secrets.owner_email
+    puts @contact.email
     mail(to: Rails.application.secrets.owner_email, from: @contact.email, :subject => "Website Contact")
     
   end
